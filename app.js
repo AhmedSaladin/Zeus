58357b30a31a526f2c07d8812d181142387a9a47
Ahmed Saladin <as.yuri74@gmail.com>
2019-07-14 23:45:07 +0200

    Test weather api

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 //Init weather object
-const weather = new Weather;
+const weather = new Weather('awsim','eg');
+
+weather.getWeather()
+.then(result => console.log(result))
+.catch(err=> console.log(err));
--- weather.js
+++ weather.js
@@ -1,26 +1,22 @@
 class Weather {
-
-    constructor(city, countryCode) {
-        this.apiKey = '7480d720b3e760da95cb2ee794c19907';
-        this.city = city;
-        this.countryCode = countryCode;
-
+    constructor(city,countryCode) {
+      this.apiKey = '7480d720b3e760da95cb2ee794c19907';
+      this.city = city;
+      this.countryCode = countryCode;
     }
-
-    //Fetch weather from API
+  
+    // Fetch weather from API
     async getWeather() {
-        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&APPID=${this.apiKey}`);
-
-        const responseData = await response.json();
-
-        return responseData.current_observation
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`);
+  
+      const responseData = await response.json();
+  
+      return responseData;
     }
-
-    //Change Weather Location
-    changeLocation(city, countryCode) {
-        this.city = city;
-        this.countryCode = countryCode;
-
+  
+    // Change weather location
+    changeLocation(city,countryCode) {
+      this.city = city;
+      this.countryCode = countryCode;
     }
-    
-}
+  }

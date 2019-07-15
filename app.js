809fb17728fd2d176cc9cbb30cbc1b0bfaf17b0c
Ahmed Saladin <as.yuri74@gmail.com>
2019-07-15 16:18:41 +0200

    update UI

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 //Init weather object
-const weather = new Weather('awsim','eg');
+const weather = new Weather('awsim', 'eg');
+//Init UI 
+const ui = new UI();
 
+//Get Weather on DOM Load
+document.addEventListener('DOMContentLoad', getWeather);
+
+function getWeather() {
+  
+}
 weather.getWeather()
-.then(result => console.log(result))
-.catch(err=> console.log(err));
+.then(result => {
+
+    console.log(result);
+    ui.paint(result);
+})
+.catch(err => console.log(err));
--- weather.js
+++ weather.js
@@ -1,22 +1,24 @@
 class Weather {
-    constructor(city,countryCode) {
-      this.apiKey = '7480d720b3e760da95cb2ee794c19907';
-      this.city = city;
-      this.countryCode = countryCode;
-    }
-  
-    // Fetch weather from API
-    async getWeather() {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`);
-  
-      const responseData = await response.json();
-  
-      return responseData;
-    }
-  
-    // Change weather location
-    changeLocation(city,countryCode) {
-      this.city = city;
-      this.countryCode = countryCode;
-    }
+  constructor(city, countryCode) {
+    this.apiKey = '49fbf8d48aa14b3d930123820191507';
+    this.city = city;
+    this.countryCode = countryCode;
   }
+
+  // Fetch weather from API
+  async getWeather() {
+
+    const response = await fetch(`https://api.apixu.com/v1/current.json?key=${this.apiKey}&q=${this.city},${this.countryCode}`)
+
+    const responseData = await response.json();
+
+    return responseData;
+
+  }
+
+  // Change weather location
+  changeLocation(city, countryCode) {
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+}

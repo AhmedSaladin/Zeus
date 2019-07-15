3b8847d5c5680e5bb036e2488d4b82333de21db3
Ahmed Saladin <as.yuri74@gmail.com>
2019-07-15 18:10:40 +0200

    Set Local Storage

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+//Init Storage
+const storage = new Storage();
+
+//Get Stored location data 
+const weatherLocation = storage.getLocationData();
+
 //Init weather object
-const weather = new Weather('awsim', 'eg');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
 //Init UI 
 const ui = new UI();
+
+
 
 //Get Weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    // Change location
+    weather.changeLocation(city, state);
+
+    //Set Location in LS
+    storage.setLocationData(city, state);
+
+    //Get and Display weather
+    getWeather()
+
+    //Close model
+    $('#locModal').modal('hide');
+});
+
 function getWeather() {
     weather.getWeather()
-    .then(result => {
-    
-        ui.paint(result);
-    })
-    .catch(err => console.log(err));
+        .then(result => {
+
+            ui.paint(result);
+        })
+        .catch(err => console.log(err));
 }

cc0770058c5bd2e4a84b44cfece0c6c848ebbfbc
Ahmed Saladin <as.yuri74@gmail.com>
2021-03-27 18:12:06 +0200

    adding cors

--- weather.js
+++ weather.js
@@ -1,17 +1,15 @@
 class Weather {
   constructor(city, country) {
-    this.apiKey ='3b132185944915881b22aef3bd9e2221';
+    this.apiKey = '3b132185944915881b22aef3bd9e2221';
     this.city = city;
     this.country = country;
   }
 
   // Fetch weather from API
   async getWeather() {
-
-    const response = await fetch(`http://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city},${this.country}`)
+    const response = await fetch(`http://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city},${this.country}`, { mode: 'cors' })
 
     const responseData = await response.json();
-    console.log(responseData);
 
     return responseData;
 
